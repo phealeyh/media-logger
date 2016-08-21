bd92349876ecaeb736f26268447c13ae6def9d70
Phealey Hang <Phealey.Hang@student.uts.edu.au>
2016-08-21 19:00:07 +1000

    added string

--- src/Constants.java
+++ src/Constants.java
@@ -17,4 +17,5 @@ public class Constants {
     public static final String SAVE_AS = "Save As";
     public static final String LOAD = "Load";
     public static final String EXIT = "Close";
+    public static final String KEVIN = "kevin";
 }
