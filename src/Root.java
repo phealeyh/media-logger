e05885111f2c18342f6026311527d348141a3fd7
kvn247 <kvn24.7@live.com.au>
2016-08-21 19:39:20 +1000

    Added debug message in Root class

--- src/Root.java
+++ src/Root.java
@@ -5,6 +5,7 @@ public class Root {
     public static void main(String[] args){
         new Window();;
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 
 }
