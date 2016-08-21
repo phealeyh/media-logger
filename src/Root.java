dc3a1ca7538d06d26be94134b3308baae50a523e
kvn247 <kvn24.7@live.com.au>
2016-08-21 19:42:37 +1000

    Removed debug messages in Root class

--- src/Root.java
+++ src/Root.java
@@ -4,8 +4,6 @@ public class Root {
 
     public static void main(String[] args){
         new Window();;
-        System.out.println("Hello World");
-        System.out.println("Hello World");
     }
 
 }
