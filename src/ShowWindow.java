d8a1108eaf68613b31715929c37be84769328879
Phealey Hang <Phealey.Hang@student.uts.edu.au>
2016-09-18 13:26:23 +1000

    refactored constructPlot function so that it is a static function that is only implemented once.

--- src/ShowWindow.java
+++ src/ShowWindow.java
@@ -62,45 +62,10 @@ public class ShowWindow extends JFrame{
             //add plot information here
             box.add(plot);
             //add the overview of the plot
-            box = constructPlot(series.getOverview().split(" "),box);
+            box = PlotConstructor.constructPlot(series.getOverview().split(" "),box);
             add(box, BorderLayout.NORTH);
         }
 
-        /* Method: constructPlot
-         * -----------------------
-         * This method will be called to help construct
-         * the Jlabels in a readable format so that it doesn't
-         * span over the width of the window. It does this by controlling
-         * the amount of words that are going to be added by having a count variable
-         * that adds the appended sentences so far and resets the count variable and the
-         * appended string. When the final string of the array has been hit and the
-         * counter is less than 11, then it will add in whatever it has
-         * left in the appended string.
-         */
-
-
-        private Box constructPlot(String strings[], Box box){
-            int count = 0;
-            String text = "";
-            for(int i = 0; i < strings.length; i++){
-                text += strings[i] + " ";
-                JLabel label = new JLabel(text);
-                if(count == 11){
-                    label.setText(label.getText());
-                    count = 0;
-                    label.setFont(label.getFont().deriveFont(10.0f));
-                    text = "";
-                    box.add(label);
-                }
-                else if(i == (strings.length - 1)){
-                    label.setText(text);
-                    label.setFont(label.getFont().deriveFont(10.0f));
-                    box.add(label);
-                }
-                count++;
-            }
-            return box;
-        }
 
     }
 
