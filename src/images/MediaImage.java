be0480657a609af36edf76d2a517a99baee783f4
Phealey Hang <Phealey.Hang@student.uts.edu.au>
2016-11-11 22:37:10 +1100

    image width reduced

--- src/images/MediaImage.java
+++ src/images/MediaImage.java
@@ -8,12 +8,14 @@ import java.net.URL;
  * Created by phealeyhang on 14/10/2016.
  */
 public class MediaImage {
+
     private Image image = null;
+
     public MediaImage(String path){
         try{
             URL url = new URL(path);
             image = ImageIO.read(url);
-            image = image.getScaledInstance(185,215,Image.SCALE_FAST);
+            image = image.getScaledInstance(150,215,Image.SCALE_FAST);
         }
         catch(java.io.IOException f){
             f.printStackTrace();
